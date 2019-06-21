1a77431282325f24f20425d7d8a06b099f60ff85
allen <ququ250@qq.com>
2019-06-21 13:47:27 +0800

    新增统计数据代码

--- manager/views.py
+++ manager/views.py
@@ -10,7 +10,8 @@ from manager.models import *
 from storybook_sever import api
 from manager.managerCommon import *
 from storybook_sever.api import Api
-
+from datetime import datetime
+from django.db.models import Count
 
 def admin(request):
     """
@@ -25,16 +26,15 @@ def login(request):
     """登录模块"""
     # 前端传入token ， 先在缓存查找， 如果没有（调用接口查询），
     token = request.META.get('HTTP_TOKEN')
-    user_data = cache.get(token)
+    user_data = caches['default'].get(token)
     # 缓存有数据,则在缓存中拿数据，登录日志添加新数据
     if user_data:
         try:
             # 获取缓存用户信息
-            user_info = cache.get(token)
-            user = User.objects.filter(userID=user_info.get('userId', '')).first()
-
-            role = user.get('role')
-            status = user.get('status')
+            user_data = caches['default'].get(token)
+            user = User.objects.filter(userID=user_data.get('userID', '')).only('userID').first()
+            role = user.roles
+            status = user.status
             if status == 'forbbiden_login':
                 return http_return(400, '此用户被禁止登录')
                 # 获取登录ip
@@ -52,17 +52,20 @@ def login(request):
         except Exception as e:
             logging.error(str(e))
             return http_return(400, '登陆失败')
+        return http_return(200, '登陆成功', {'roles': role})
     # 缓存中没有数据
     if not user_data:
         api = Api()
         # 校验前端传过来的token值
         user_info = api.check_token(token)
+
         if not user_info:
             return http_return(400, '未获取到用户信息')
         else:
 
             # 用户表中是否有该用户
-            user = User.objects.filter(userID=user_info.get('userId', '')).first()
+            userID = user_info.get('userId', '')
+            user = User.objects.filter(userID=userID).only('userID').first()
             # 状态 normal  destroy  forbbiden_login  forbbiden_say
             if user and user.status == 'destroy':
                 return http_return(400, '无此用户')
@@ -72,11 +75,10 @@ def login(request):
 
             # 当前表中没有此用户信息则在数据库中创建
             if not user:
-                user_uuid = get_uuid()
                 user = User(
-                    uuid=user_uuid,
+                    uuid=get_uuid(),
                     tel=user_info.get('phone', ''),
-                    userID=user_info.get('userId', ''),
+                    userID=userID,
                     username=user_info.get('wxNickname', ''),
                     roles="adminUser",
                     # roles="normalUser",
@@ -90,7 +92,8 @@ def login(request):
                 except Exception as e:
                     logging.error(str(e))
                     return http_return(400, '保存失败')
-            user = User.objects.filter(Q(userID=user_info.get('userId', '')) and ~Q(status='forbbiden_login')).first()
+            user = User.objects.filter(userID=userID).only('userID').first()
+            print(user.uuid)
             role = user.roles
             # 获取登录ip
             loginIp = get_ip_address(request)
@@ -105,31 +108,49 @@ def login(request):
                 ipAddr=loginIp,
                 userUuid=user
             )
-            loginLog.save()
+            try:
+                with transaction.atomic():
+                    loginLog.save()
+            except Exception as e:
+                logging.error(str(e))
+                return http_return(400, '保存日志失败')
 
-    return http_return(200, '登陆成功', {'roles': role, 'userInfo': user_info})
+    return http_return(200, '登陆成功', {'roles': role})
 
 
 def total_data(request):
     if request.method == 'POST':
-        startTime = request.POST.get('startTime', 0)
-        endTime = request.POST.get('endTime', 0)
-        currentDate = datetime.datetime.now().date()
-
-        if startTime and endTime:
-            startTime = unix_time_to_datetime(int(startTime)).date()
-            endTime = unix_time_to_datetime(int(endTime)).date()
-            totalUsers1 = User.objects.filter(createTime__gte=startTime).count()
-            totalUsers2 = User.objects.filter(createTime__lte=endTime).count()
-            totalUsers = User.objects.filter(Q(createTime__gte=startTime) and Q(createTime__lte=endTime)).count()
+        try:
+            startTimestamp = int(request.POST.get('startTime', 0))
+            endTimestamp = int(request.POST.get('endTime', 0))
+        except Exception as e:
+            logging.error(str(e))
+            http_return(400, '参数有误无法查询')
+        if endTimestamp < startTimestamp or endTimestamp < 0 or startTimestamp < 0:
+            http_return(400, '参数有误无法查询')
+        if startTimestamp and endTimestamp:
+            # 给定时间查询
+            startTime = datetime.fromtimestamp(startTimestamp)
+            endTime = datetime.fromtimestamp(endTimestamp)
+            t1 = datetime(startTime.year, startTime.month, startTime.day)
+            t2 = datetime(endTime.year, startTime.month, startTime.day, 23, 59, 59, 999999)
+            # 用户总人数
+            totalUsers = User.objects.exclude(status='destroy').count()
+            # 音频总数
+            totalWorks = Works.objects.all().count()
+            # 专辑总数
+            totalAlbums = Album.objects.all().count()
+            # 新增用户人数
+            newUsers = User.objects.filter(Q(createTime__range=(t1, t2))).count()
+            # 活跃用户人数
+            activityUsers = LoginLog.objects.filter(Q(createTime__range=(t1, t2))).only('userUuid_id').distinct().count()
+            a = LoginLog.objects.values('userUuid_id').annotate(Count('userUuid_id')).count()
 
         else:
-            # 返回所有数据
+            # 没有给定参数返回最近一个月所有数据
             totalUsers = User.objects.filter(~Q(status='destroy')).count()
             # 按时间范围搜索
             # 首先获取时间范围，然后再查询
-            # 用户总人数
-            # totalUsers =
             # 新增用户人数
             # 活跃用户人数
             # 故事总数
@@ -138,8 +159,8 @@ def total_data(request):
         return http_return(200, 'OK',
                            {
                                'totalUsers': totalUsers,
-                               'newUsers': 12,
-                               'activityUsers': 23,
+                               'newUsers': newUsers,
+                               'activityUsers': activityUsers,
                                'totalStories': 23,
                                'totalTemplateStroies': 23
                            })

9ec489a173e305fd4ac78a483ed7a2ea0ca766bc
2890353332@qq.com <2890353332@qq.com>
2019-06-28 11:32:19 +0800

    完成活动展示

--- api/urls.py
+++ api/urls.py
@@ -28,21 +28,23 @@ urlpatterns = [
 
     path('index/banner', views.index_banner, name='index_banner'),  # 首页轮播图
     path('index/list', views.index_list, name='index_list'),  # 首页展示列表
+    path('index/more', views.index_more, name='index_more'),  # 展示更多
 
     path('search/all', views.search_all, name='search_all'),  # 搜索
     path('search/audiostory', views.search_audio, name='search_audio'),  # 搜索模板音频
     path('search/user', views.search_user, name='search_user'),  # 搜索主播
     path('search/hotkeyword', views.search_hot, name='search_hot'),  # 热搜词
 
-    path('audiostory/categroy/detail', views.audiostory_categroy_detail, name='audiostory_categroy_detail'),  # 分类展示
-    path('audiostory/categroy/list', views.index_categroy_list, name='index_categroy_list'),  # 分类标签展示
-    path('audiostory/categroy/result', views.index_categroy_result, name='index_categroy_result'),  # 筛选结果
-    path('audiostory/categroy/audiostory', views.index_categroy_audiostory, name='index_categroy_audiostory'),  # 筛选结果
-    path('audiostory/categroy/user', views.index_categroy_user, name='index_categroy_user'),  # 筛选结果
+    path('audiostory/category/detail', views.audiostory_category_detail, name='audiostory_category_detail'),  # 分类展示
+    path('audiostory/category/list', views.index_category_list, name='index_category_list'),  # 分类标签展示
+    path('audiostory/category/result', views.index_category_result, name='index_category_result'),  # 筛选结果
+    path('audiostory/category/audiostory', views.index_category_audiostory, name='index_category_audiostory'),  # 筛选结果
+    path('audiostory/category/user', views.index_category_user, name='index_category_user'),  # 筛选结果
 
     path('audiostory/praise', views.audiostory_praise, name='audiostory_praise'),  # 点赞作品
     path('audiostory/cancelpraise', views.audiostory_cancel_praise, name='audiostory_cancel_praise'),  # 取消点赞
 
     path('activity/detail', views.activity_detail, name='activity_detail'),  # 活动详情
+    path('activity/ranklist', views.activity_rank, name='activity_rank'),  # 活动排行
 
 ]

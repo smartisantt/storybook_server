f5efd293a29b5ecb3eed626af572227e997a4e9b
York <2890353332@qq.com>
2019-09-04 17:53:05 +0800

    完成消息接口

--- api/views.py
+++ api/views.py
@@ -206,7 +206,7 @@ def recording_send(request):
         return http_return(400, '请输入标题')
     # 审核标题
     text = TextAudit()
-    if not text.work_on(name):
+    if not text.work_on(str(name)):
         return http_return(400, "你输入的标题包含非法信息，请重新输入")
     if remarks:
         if not text.work_on(remarks):

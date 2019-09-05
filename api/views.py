40fda2b7f3fb01da5170e73c233a8f148685d029
York <2890353332@qq.com>
2019-09-05 17:01:12 +0800

    完成消息接口

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from urllib.parse import urljoin
 
 from api.apiCommon import *
 from api.ssoSMS.sms import send_sms
+from common.MyJpush import jpush_platform_msg
 from common.common import *
 from common.mixFileAPI import MixAudio
 from common.textAPI import TextAudit
@@ -311,7 +312,7 @@ def become_fans(request):
     if not data:
         return http_return(400, '请求错误')
     uuid = data.get('uuid', '')
-    type = data.get('type', '')
+    type = data.get('type', '')  # 1取消关注 2关注
     selfUuid = data['_cache']['uuid']
     if selfUuid == uuid:
         return http_return(400, '不能自己关注自己')
@@ -339,6 +340,16 @@ def become_fans(request):
             except Exception as e:
                 logging.error(str(e))
                 return http_return(400, '关注失败')
+            title = "关注提醒"
+            content = selfUser.nickName + "关注了" + user.nickName
+            extras = {"type": 1, "unread": 1}
+            alias = []
+            alias.append(uuid)
+            # 推送点赞信息
+            try:
+                jpush_platform_msg(title, content, extras, alias)
+            except Exception as e:
+                logging.error(str(e))
         return http_return(200, '关注成功')
 
 

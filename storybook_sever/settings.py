5b578e6f2e84a65501de6e45f2ef6d5d6daccdf1
York <2890353332@qq.com>
2019-09-05 09:52:06 +0800

    完成消息接口

--- storybook_sever/settings.py
+++ storybook_sever/settings.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Django settings for storybook_sever project.
 
@@ -350,6 +352,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
+
+DEFAULT_CHARSET = "UTF-8"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/

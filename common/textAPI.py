6f848fc546c53cfb9c425ac260ee96e2080f3db9
York <2890353332@qq.com>
2019-09-05 09:39:22 +0800

    完成消息接口

--- common/textAPI.py
+++ common/textAPI.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging
+
 import requests
-# import sys
-# reload(sys)
-# sys.setdefaultencoding("utf8")
+import sys
+from imp import reload
+reload(sys)
+sys.setdefaultencoding("utf8")
 
 
 class TextAudit(object):

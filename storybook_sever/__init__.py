dd4e0bb10b13158817b016c1cd6e72a20a56f9fa
allen <ququ250@qq.com>
2019-09-06 19:38:34 +0800

    取消celery

--- storybook_sever/__init__.py
+++ storybook_sever/__init__.py
@@ -1,6 +1,6 @@
-from __future__ import absolute_import
-# 引入celery实例对象
-from .celery import app as celery_app
+# from __future__ import absolute_import
+# # 引入celery实例对象
+# from .celery import app as celery_app
 
 import pymysql
 pymysql.install_as_MySQLdb()
--- storybook_sever/celery.py
+++ storybook_sever/celery.py
@@ -1,17 +1,17 @@
-from __future__ import absolute_import
-
-import os
-from celery import Celery
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storybook_sever.settings')
-
-app = Celery('storybook_sever')
-
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+# from __future__ import absolute_import
+#
+# import os
+# from celery import Celery
+# from django.conf import settings
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storybook_sever.settings')
+#
+# app = Celery('storybook_sever')
+#
+# app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+#
+#
+# @app.task(bind=True)
+# def debug_task(self):
+#     print('Request: {0!r}'.format(self.request))

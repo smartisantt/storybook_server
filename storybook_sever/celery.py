f5f94bf2ee79f0445bb3ffcca0d2b68d809ae375
York <2890353332@qq.com>
2019-09-06 17:47:44 +0800

    修改评论列表

--- storybook_sever/celery.py
+++ storybook_sever/celery.py
@@ -1,17 +1,16 @@
-from __future__ import absolute_import
-
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
+from django.utils import timezone
 
+# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storybook_sever.settings')
 
-app = Celery('app')
+app = Celery('storybook_sever')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.now = timezone.now
 
+# Load task modules from all registered Django app configs.
 
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+app.autodiscover_tasks()
